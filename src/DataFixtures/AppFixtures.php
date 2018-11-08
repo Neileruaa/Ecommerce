bc41490c45248711f4722922b1b789791d829006
aheitzm2 <antoinedebourb@gmail.com>
2018-11-08 13:46:02 +0100

    formulaires(user,produit,typeproduit)+entités+fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Panier;
+use App\Entity\Produit;
+use App\Entity\TypeProduit;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use MongoDB\Driver\Manager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $this->addProduits($manager);
+        $this->addUsers($manager);
+        //$this->addUsers($manager);
+    }
+    public function addProduits(ObjectManager $manager){
+        $vetement=new TypeProduit();
+        $vetement->setLibelle("vetement");
+        $bureautique=new TypeProduit();
+        $bureautique->setLibelle("bureautique");
+        $meuble=new TypeProduit();
+        $meuble->setLibelle("meuble");
+        $produits=[
+            ["nom"=>"Tshirt", "prix"=>29.99, "photo"=>"test.png", "disponible"=>true, "stock"=>12, "typeProduit_id"=>$vetement],
+            ["nom"=>"Stylo", "prix"=>29.99, "photo"=>"test.png", "disponible"=>true, "stock"=>3, "typeProduit_id"=>$bureautique],
+            ["nom"=>"Armoire", "prix"=>29.99, "photo"=>"test.png", "disponible"=>false, "stock"=>0, "typeProduit_id"=>$meuble]
+        ];
+        $manager->persist($vetement);
+        $manager->persist($bureautique);
+        $manager->persist($meuble);
+        foreach ($produits as $produit){
+            $new_produit=new Produit();
+            $new_produit->setNom($produit["nom"]);
+            $new_produit->setPrix($produit["prix"]);
+            $new_produit->setPhoto($produit['photo']);
+            $new_produit->setDisponible($produit["disponible"]);
+            $new_produit->setStock($produit["stock"]);
+            $new_produit->setTypeProduitId($produit["typeProduit_id"]);
+
+            $manager->persist($new_produit);
+        }
+        $manager->flush();
+    }
+    public function addUsers(ObjectManager $manager){
+        $user=new User();
+        $admin=new User();
+        $panier_user=new Panier();
+        $panier_admin=new Panier();
+
+        $user->setUserName("antoine");
+        $user->setPassword("test");
+        $user->setEmail("test@gmail.com");
+        $user->setRoles("['ROLE_USER']");
+        $user->setActive(true);
+        $user->setPanier($panier_user);
+
+        $admin->setUserName("admin");
+        $admin->setPassword("admin");
+        $admin->setEmail("admin@gmail.com");
+        $admin->setRoles("['ROLE_ADMIN']");
+        $admin->setActive(true);
+        $admin->setPanier($panier_admin);
+
+        $manager->persist($user);
+        $manager->persist($admin);
+        $manager->flush();
+
+    }
+}
---
+++ src/Entity/Produit.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProduitRepository")
+ */
+class Produit
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $photo;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $disponible;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $stock;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\TypeProduit")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $typeProduit_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    public function setPrix($prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getDisponible(): ?bool
+    {
+        return $this->disponible;
+    }
+
+    public function setDisponible(bool $disponible): self
+    {
+        $this->disponible = $disponible;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function getTypeProduitId(): ?TypeProduit
+    {
+        return $this->typeProduit_id;
+    }
+
+    public function setTypeProduitId(?TypeProduit $typeProduit_id): self
+    {
+        $this->typeProduit_id = $typeProduit_id;
+
+        return $this;
+    }
+}
