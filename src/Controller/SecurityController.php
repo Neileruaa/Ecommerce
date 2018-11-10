6c6d002ed263f3afd543d47e95645db473e57cc2
Aurélien <aureliendrey@gmail.com>
2018-11-10 22:31:10 +0100

    Captcha google (v2)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,7 @@ use App\Entity\Panier;
 use App\Entity\User;
 use App\Form\RegistrationType;
 use Doctrine\Common\Persistence\ObjectManager;
+use ReCaptcha\ReCaptcha;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,29 +27,36 @@ class SecurityController extends Controller
 	                             ObjectManager $manager,
 	                             UserPasswordEncoderInterface $encoder,
 								 $typeCompte) {
+		$recaptcha = new ReCaptcha('6Ld153kUAAAAAHSMD1u2Vc55NXhkEg1hXFMzYiGP');
+		$resp = $recaptcha->verify($request->request->get('g-recaptcha-response'), $request->getClientIp());
+		$errors = array();
 		$user = new User();
 		$form = $this->createForm(RegistrationType::class, $user);
 
 		$form->handleRequest($request);
 
 		if ($form->isSubmitted() && $form->isValid() ){
-			$hash = $encoder->encodePassword($user, $user->getPassword());
-			$user->setPassword($hash);
-			$user->setActive(true);
-			$panier = new Panier();
-			$user->setPanier($panier);
-			$user->setRoles('ROLE_USER');
-			if ($typeCompte == 'admin'){
-				$user->setRoles('ROLE_ADMIN');
+			if ($resp->isSuccess()) {
+				$hash = $encoder->encodePassword($user, $user->getPassword());
+				$user->setPassword($hash);
+				$user->setActive(true);
+				$panier = new Panier();
+				$user->setPanier($panier);
+				$user->setRoles('ROLE_USER');
+				if ($typeCompte == 'admin') {
+					$user->setRoles('ROLE_ADMIN');
+				}
+				$manager->persist($panier);
+				$manager->persist($user);
+				$manager->flush();
+				return $this->redirectToRoute('Security.login');
+			}else{
+				$errors = $resp->getErrorCodes();
 			}
-			$manager->persist($panier);
-			$manager->persist($user);
-			$manager->flush();
-
-			return $this->redirectToRoute('Security.login');
 		}
 		return $this->render('security/registration.html.twig',[
-			'form'=>$form->createView()
+			'form'=>$form->createView(),
+			'errors'=>$errors
 		]);
     }
 
