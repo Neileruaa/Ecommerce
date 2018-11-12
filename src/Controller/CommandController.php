58708ab0c257f590610fda0eedf268dacfc49f26
Aurélien <aureliendrey@gmail.com>
2018-11-12 11:57:42 +0100

    Twig file

--- src/Controller/CommandController.php
+++ src/Controller/CommandController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CommandController extends AbstractController
-{
+class CommandController extends AbstractController {
 
 }
